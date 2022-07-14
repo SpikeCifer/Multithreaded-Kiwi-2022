e86526e029e65ffc65579d5cd87fe7b7bd900fdd
spikeCifer <stelioszapp+dev@gmail.com>
2022-07-14 13:54:25 +0300

    Renamed the kiwi-source -> src

--- kiwi-source/engine/hash.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HASH_H__
-#define __HASH_H__
-
-#include <sys/types.h>
-#include <stdint.h>
-
-uint32_t hash(const char* data, size_t n, uint32_t seed);
-
-#endif
--- kiwi-source/engine/variant.h
+++
@@ -1,9 +0,0 @@
-#ifndef __VARIANT_H__
-#define __VARIANT_H__
-
-#include "buffer.h"
-
-typedef enum {ADD,DEL} OPT;
-typedef Buffer Variant;
-
-#endif
